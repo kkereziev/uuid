501b7efbf37107d57287df92bcf245d61b98a7b2
kkereziev <kkereziev97@gmail.com>
2025-01-05 20:36:24 +0200

    feat: init

---
+++ .zig-cache/o/d8412458b082abb5508ec055e1b7a2f1/dependencies.zig
@@ -0,0 +1,12 @@
+pub const packages = struct {
+    pub const @"1220e1bd4b2c644bba7e8f2881019ad20c7e855d708a018ec47b6e032a04739736de" = struct {
+        pub const build_root = "/home/hydra/.cache/zig/p/1220e1bd4b2c644bba7e8f2881019ad20c7e855d708a018ec47b6e032a04739736de";
+        pub const build_zig = @import("1220e1bd4b2c644bba7e8f2881019ad20c7e855d708a018ec47b6e032a04739736de");
+        pub const deps: []const struct { []const u8, []const u8 } = &.{
+        };
+    };
+};
+
+pub const root_deps: []const struct { []const u8, []const u8 } = &.{
+    .{ "string_builder", "1220e1bd4b2c644bba7e8f2881019ad20c7e855d708a018ec47b6e032a04739736de" },
+};
---
+++ src/UUID.zig
@@ -0,0 +1,75 @@
+const std = @import("std");
+const StringBuilder = @import("string_builder");
+const Self = @This();
+
+pub const Nil = Self{ ._bytes = [_]u8{0x0} ** 16 };
+
+// Indices in the UUID string representation for each byte.
+const encoded_pos = [16]u8{ 0, 2, 4, 6, 9, 11, 14, 16, 19, 21, 24, 26, 28, 30, 32, 34 };
+const hex = "0123456789abcdef";
+const hex_to_nibble = [_]u8{
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   255, 255, 255, 255, 255, 255,
+    255, 10,  11,  12,  13,  14,  15,  255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 10,  11,  12,  13,  14,  15,  255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+};
+
+_bytes: [16]u8,
+
+pub fn newV4() Self {
+    var uuid = Self{ ._bytes = undefined };
+
+    std.crypto.random.bytes(&uuid._bytes);
+
+    uuid._bytes[6] = (uuid._bytes[6] & 0x0f) | 0x40;
+    uuid._bytes[8] = (uuid._bytes[8] & 0x3f) | 0x80;
+
+    return uuid;
+}
+
+pub fn toString(self: Self, string: []u8) void {
+    var buf: [36]u8 = undefined;
+
+    buf[8] = '-';
+    buf[13] = '-';
+    buf[18] = '-';
+    buf[23] = '-';
+    inline for (encoded_pos, 0..) |i, j| {
+        buf[i + 0] = hex[self._bytes[j] >> 4];
+        buf[i + 1] = hex[self._bytes[j] & 0x0f];
+    }
+
+    @memcpy(string, buf[0..]);
+}
+
+test {
+    std.testing.refAllDecls(@This());
+}
+
+const t = std.testing;
+
+test "generate new v4 uuid and print it via string" {
+    var buf: [36]u8 = undefined;
+    newV4().toString(&buf);
+
+    std.debug.print("{s}\n", .{buf});
+}
+
+test "print nil uuid v4" {
+    var buf: [36]u8 = undefined;
+    Nil.toString(&buf);
+
+    try t.expectEqualStrings("00000000-0000-0000-0000-000000000000", &buf);
+}
